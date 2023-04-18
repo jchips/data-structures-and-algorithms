ef221e40f2e0bb845c27caa8660a8939e8c30d5f
Jchips <20979096+Jchips@users.noreply.github.com>
2023-04-17 19:38:00 -0700

    attempting challenge 7

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -149,6 +149,19 @@ Return the resulting output array.
 
 const fizzbuzz = (arr) => {
   // Solution code here...
+  let outputArray = [];
+  arr.forEach(num => {
+    if (num % 3 === 0) {
+      outputArray.push('Fizz');
+    } else if (num % 5 === 0) {
+      outputArray.push('Buzz');
+    } else if (num % 15 === 0) {
+      outputArray.push('Fizz Buzz');
+    } else {
+      outputArray.push(num);
+    }
+  });
+  return outputArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -202,7 +215,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test('It should print out messages or numbers', () => {
