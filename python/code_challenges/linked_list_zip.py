2422922338dd8a086b70ee0ad64fdb1634b4848a
Jchips <20979096+Jchips@users.noreply.github.com>
2025-04-11 21:28:10 -0700

    finished python challenge 8

--- python/code_challenges/linked_list_zip.py
+++ python/code_challenges/linked_list_zip.py
@@ -1,2 +1,16 @@
 def zip_lists(a, b):
-    pass
+    a_current = a.head
+    b_current = b.head
+    if not a_current:
+        return b
+    while a_current and b_current:
+        a_next = a_current.next
+        b_next = b_current.next
+        a_current.next = b_current
+        b_current.next = a_next
+        a_current = a_next
+        b_current = b_next
+    while b_current:
+        a.append(b_current)
+        b_current = b_current.next
+    return a
