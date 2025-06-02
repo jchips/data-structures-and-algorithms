3986cfd8c15259d3f3a279bba5792771073cf80e
Jchips <20979096+Jchips@users.noreply.github.com>
2025-06-01 21:21:05 -0700

    added stack implementation in python

---
+++ python/data_structures/node.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return f'Node({self.value}) -> {self.next_node}'
--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,45 @@
+from data_structures.node import Node
+
+
 class Stack:
     """
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, top=None):
+        self.top = top
 
-    def some_method(self):
-        # method body here
-        pass
+    def __str__(self):
+        string = ''
+        current = self.top
+        while current:
+            string += f'{{ {current.value} }} -> '
+            current = current.next_node
+        else:
+            string += 'NULL'
+        return string
+
+    def push(self, value):
+        top = self.top if self.top else None
+        self.top = Node(value, top)
+
+    def pop(self):
+        if self.top is None:
+            raise EmptyStackError
+        top_value = self.top.value
+        self.top = self.top.next_node
+        return top_value
+
+    def peek(self):
+        if self.top is None:
+            raise EmptyStackError
+        return self.top.value
+
+    def is_empty(self):
+        if self.top is None:
+            return True
+        return False
+
+
+class EmptyStackError(Exception):
+    pass
